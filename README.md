a46c73a44734d3156c7e0205122b084202365f0e
Harald Weidner <hweidner@gmx.net>
2018-02-09 23:52:57 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,88 @@
+Parallel Sync - parallel recursive copying of directories
+=========================================================
+
+psync is a tool which copies a directory recursively to another directory.
+Unlike "cp -r", which walks through the files and subdirectories in sequential
+order, psync copies several files concurrently by using threads.
+
+Usage
+-----
+
+psync is invoked as follows:
+
+	psync [-verbose] [-threads <num>] source destination
+	
+	-verbose        - verbose mode, prints the current workload to STDOUT
+	-threads <num>  - number of concurrent threads, 1 <= <num> <= 1024, default 16
+	source          - source directory
+	destination     - destination directory
+
+Example
+-------
+
+Copy all files and subdirectories from /data/src into /data/dest.
+
+	psync -n 8 /data/src /data/dest
+
+/data/src and /data/dest must exist and must be directories.
+
+Why should I use it
+-------------------
+
+A recursive copy of a directory can be a throughput bound or latency bound
+operation, depending on the size of files and characteristics of the source
+and/or destination file system. When copying between standard file systems on
+two local hard disks, the operation is typically throughput bound, and copying
+in parallel has no performance advantage over copying sequentially. In this
+case, you have a bunch of options, including "cp -r" or "rsync".
+
+However, when copying from or to network file systems (NFS, CIFS), WAN storage
+(WebDAV, external cloud services), distributed file systems (GlusterFS, CephFS)
+or file systems that live on a DRBD device, the latency for each file access is
+often limiting performance factor. With sequential copies, the operation can
+consume lots of time, although the bandwidth is not saturated. In this case, it
+can make up a significant performance boost if the files are copied in parallel.
+
+Where psync should not be used
+------------------------------
+
+Parallel copying is typically not so useful when copying between local or
+very fast hard disks. psync can be used there, and with a moderate concurrency
+level like 2..5, it can be slightly faster than a sequential copy.
+
+Parallel copying should never be used when duplicating directories on the same
+physical hard disk. Even sequential copies suffer from the frequent hard disk head
+movements which are needed to read and write concurrently on/to the same disk.
+Parallel copying even increases the amount of head movements.
+
+How it works
+------------
+
+psync uses goroutines for copying files in parallel. By default, 16 copy workers
+are spawned as goroutines, but the number can be adjusted with the -threads switch.
+
+Each worker waits for a directory to be submitted. It then handles all the
+directory entries sequentially. Files a copied one by one to the destination
+directory. When subdirectories are discovered, they are created on the destination
+side. Traversal of the subdirecory is then submitted to other workers and thus done
+in parallel to the current workload.
+
+Limits and TODOs
+----------------
+
+psync currently can only handle directories, regular files, and symbolic links.
+Other filesystem entries like devices, sockets or named pipes are silently ignored.
+
+psync has no support for preserving permissions, ownership, or timestamps.
+Destination files are created "as is", according to the current user, group and
+umask.
+
+psync does currently implement a simple recursive copy, like "cp -r", and not
+a versatile sync algorithm like rsync. There is no check wether a file already
+exists in the destination, nor its content and timestamps. Existing files on the
+destination side are not deleted when they don't exist on the source side.
+
+License
+-------
+
+psync is released under the terms of the GNU General Public License, version 3.
---
+++ psync.go
@@ -0,0 +1,171 @@
+// Copyright 2018 by Harald Weidner <hweidner@gmx.net>. All rights reserved.
+// Use of this source code is governed by the GNU General Public License
+// Version 3 that can be found in the LICENSE.txt file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+// Channels and Synchronization
+var (
+	dch = make(chan string, 100) // dispatcher channel - get work into work queue
+	wch = make(chan string, 100) // worker channel - get work from work queue to copy thread
+	wg  sync.WaitGroup           // waitgroup for work queue length
+)
+
+// Commandline Flags
+var (
+	threads   uint   // number of threads
+	src, dest string // source and destination directory
+	verbose   bool   // verbose flag
+)
+
+func main() {
+	// parse commandline flags
+	flags()
+
+	// Start dispatcher and copy threads
+	go dispatcher()
+	for i := uint(0); i < threads; i++ {
+		go copy_dir(i)
+	}
+
+	// start copying top level directory
+	wg.Add(1)
+	dch <- ""
+	wg.Wait() // wait for work queue to get empty
+	// time.Sleep(10 * time.Second)
+}
+
+// Function flags parses the command line flags and checks them for sanity.
+func flags() {
+	flag.UintVar(&threads, "threads", 16, "Number of threads to run in parallel")
+	flag.BoolVar(&verbose, "verbose", false, "Verbose mode")
+	flag.Parse()
+
+	if flag.NArg() != 2 || flag.Arg(0) == "" || flag.Arg(1) == "" || threads > 1024 {
+		usage()
+	}
+
+	if threads == 0 {
+		threads = 16
+	}
+	src = flag.Arg(0)
+	dest = flag.Arg(1)
+}
+
+// Funktion usage prints a message about how to use psync, and exits.
+func usage() {
+	fmt.Println("Usage: psync [options] source destination")
+	flag.Usage()
+	os.Exit(1)
+}
+
+// Function dispatcher maintains a work list of potentially arbitrary size.
+// Incoming directories (over the dispather channel) will be forwarded to a
+// copy thread through the worker channel, or stored in the work list if no
+// copy thread is available.
+func dispatcher() {
+	worklist := make([]string, 0, 1000)
+	var dir string
+	for {
+		if len(worklist) == 0 {
+			dir = <-dch
+			worklist = append(worklist, dir)
+		} else {
+			select {
+			case dir = <-dch:
+				worklist = append(worklist, dir)
+			case wch <- worklist[len(worklist)-1]:
+				worklist = worklist[:len(worklist)-1]
+			}
+		}
+	}
+}
+
+// Function copy_dir receives a directory on the worker channel and copies its
+// content from src to dest. Files are copied sequentially. If a subdirectory
+// is discovered, it is created on the destination side, and then inserted into
+// the work queue through the dispather channel.
+func copy_dir(id uint) {
+	for {
+		dir := <-wch
+		if verbose {
+			fmt.Printf("[%d] Handling directory %s%s\n", id, src, dir)
+		}
+		files, err := ioutil.ReadDir(src + dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, f := range files {
+			fname := f.Name()
+			if fname == "." || fname == ".." {
+				continue
+			}
+			if f.IsDir() {
+				os.Mkdir(dest+"/"+dir+"/"+fname, 0755)
+				wg.Add(1)
+				dch <- dir + "/" + fname
+			} else {
+				if verbose {
+					fmt.Printf("[%d] Copying %s%s/%s to %s%s/%s\n",
+						id, src, dir, fname, dest, dir, fname)
+				}
+				copy_file(dir+"/"+fname, f.Mode())
+			}
+		}
+		if verbose {
+			fmt.Printf("[%d] Finished directory %s%s\n", id, src, dir)
+		}
+		wg.Done()
+	}
+}
+
+// Function copy_file copies a file from the source to the destination directory.
+func copy_file(file string, mode os.FileMode) {
+	switch {
+
+	case mode&os.ModeSymlink != 0: // symbolic link
+		link, err := os.Readlink(src + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = os.Symlink(link, dest+file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case mode&(os.ModeDevice|os.ModeNamedPipe|os.ModeSocket) != 0:
+	// TODO: not yet implemented
+
+	default:
+		// copy regular file
+		// open source file for reading
+		rd, err := os.Open(src + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rd.Close()
+
+		// open destination file for writing
+		wr, err := os.Create(dest + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer wr.Close()
+
+		// copy data
+		_, err = io.Copy(wr, rd)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
